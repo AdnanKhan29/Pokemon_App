d5c630de6d2dc38703a53f700ed90bea39c44e53
AdnanKhan29 <102600483+AdnanKhan29@users.noreply.github.com>
2023-10-31 20:02:06 +0530

    added few components

--- react_pokedex/src/components/PokeList.js
+++ react_pokedex/src/components/PokeList.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PokeList.css";
+import PokemonCard from "./PokemonCard";
+
 function PokeList() {
-  return <div>PokeList</div>;
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  const getAllPokemons = async () => {
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=649&offset=0"
+    );
+    const data = await res.json();
+
+    async function createPokemonObject(results) {
+      const pokemonDataArray = await Promise.all(
+        results.map(async (pokemon) => {
+          const res = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+          );
+          return res.json();
+        })
+      );
+      const sortedPokemonData = pokemonDataArray.sort((a, b) => a.id - b.id);
+      setAllPokemons(sortedPokemonData);
+    }
+
+    createPokemonObject(data.results);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemons.map((pokemonStats) => (
+            <PokemonCard
+              key={pokemonStats.id}
+              id={pokemonStats.id.toString().padStart(3, "0")}
+              image={
+                pokemonStats.sprites.other["official-artwork"].front_default
+              }
+              name={pokemonStats.name.replace(/^./, (str) => str.toUpperCase())}
+              type={pokemonStats.types[0].type.name}
+              weight={pokemonStats.weight}
+              height={pokemonStats.height}
+              stats={pokemonStats.stats
+                .map((stat) => stat.base_stat)
+                .slice(0, 3)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PokeList;
